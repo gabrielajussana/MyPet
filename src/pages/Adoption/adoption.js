6c23f3d73ff73b129684f5d0608f254d5c8a378a
gabrielajussana <gabrielajussana@gmail.com>
2025-02-05 09:52:06 -0300

    fix: breeds

--- src/pages/Adoption/adoption.js
+++ src/pages/Adoption/adoption.js
@@ -1,44 +1,49 @@
-import {Container, DogList, Dog} from '../Home/styles';
-import {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom'
+import { Container, DogList, Dog } from '../Home/styles';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Parts/Header';
 import Footer from '../Parts/Footer';
 
+function Adoption() {
+  const [dogs, setDogs] = useState([]);
 
-function Adoption(){
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random/6')
+      .then(response => response.json())
+      .then(data => setDogs(data.message))
+      .catch(error => console.error('Erro ao buscar os cães:', error));
+  }, []);
 
-const[dogs, setDogs] = useState([]);
+  const getBreedFromUrl = (url) => {
+    const parts = url.split("/");
+    return parts[parts.length - 2].replace('-', ' ');
+  };
 
-  
-
- useEffect(() => {
-   fetch(`https://dog.ceo/api/breeds/image/random/6`)
-   .then(response => response.json())
-   .then(data => setDogs(data.message))
- }, [])
-
-  return(
-   <Container>
-    <Header /> 
-    <DogList>    
-         {dogs.map(dog => {
-            return(
-             <Dog>
-             <Link to={`../Details/${dog}`}><img src={`${dog}`} alt="foto do cachorro"/></Link>
-             <h2>Nome</h2>
-             <span>{dog.match(`breeds/(.*?)/`)}</span>  
-             <div className='info'>
-                  <p>Porte <span>***</span></p>
-                  <p>Idade <span>***</span></p>
-                  <p>Cidade<span>***</span></p>
-               </div> 
-             <Link to={`../form`} style={{ textDecoration: 'none' }} className='button'>ADOTE!</Link>
-             </Dog>
-            )
-         })}
-    </DogList>
-    <Footer />
-   </Container>
+  return (
+    <Container>
+      <Header />
+      <DogList>
+        {dogs.map((dog, index) => {
+          const breed = getBreedFromUrl(dog);
+          return (
+            <Dog key={index}>
+              <Link to={`../Details/${encodeURIComponent(dog)}`}>
+                <img src={dog} alt={`Foto de um ${breed}`} />
+              </Link>
+              <h2>Nome</h2>
+              <span>{breed}</span>
+              <div className='info'>
+                <p>Porte <span>***</span></p>
+                <p>Idade <span>***</span></p>
+                <p>Cidade <span>***</span></p>
+              </div>
+              <Link to='../form' style={{ textDecoration: 'none' }} className='button'>ADOTE!</Link>
+            </Dog>
+          );
+        })}
+      </DogList>
+      <Footer />
+    </Container>
   );
 }
 
